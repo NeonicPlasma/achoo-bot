94a222233c1feafc06876b53b0ea1c0efa74980d
NeonicPlasma <43083380+NeonicPlasma@users.noreply.github.com>
2018-09-23 15:15:16 +1200

    Update AchooBot.py

--- AchooBot.py
+++ AchooBot.py
@@ -20,7 +20,7 @@ async def on_ready():
 @bot.command()
 async def storymessage(ctx, message, destination):
     author = ctx.message.author
-    if author.id == 179686717534502913 or author.id == 194276511648448514 or author.id == 312996476286402560:
+    if author.id == 179686717534502913 or author.id == 194276511648448514 or author.name = 'wyatt':
         channel = ctx.message.channel_mentions[0]
         await channel.send(message)
 
